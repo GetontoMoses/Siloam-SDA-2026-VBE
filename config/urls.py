abb3cd22736e43ca985c90cfcef72ae5ab346cbf
Getonto3 <getontomoses18@gmail.com>
2026-03-20 20:32:56 +0300

    chore: update urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("vbs/", include("VBS.urls"))
+    path("vbs/", include("VBS.urls")),
+    path("Children_Ministry", include("Children_Ministry.urls"))
 ]
