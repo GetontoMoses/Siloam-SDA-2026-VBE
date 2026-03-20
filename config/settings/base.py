85ce43a40ecb93788f9da42e60efa1a4203e4a74
Getonto3 <getontomoses18@gmail.com>
2026-03-20 20:26:23 +0300

    chore: update settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "VBS",
     "corsheaders",
     "rest_framework",
+    "VBS",
+    "Accounts"
 ]
 
 MIDDLEWARE = [
