bec98ff9287b5e598be58c6a2efda91a170d6768
Getonto3 <getontomoses18@gmail.com>
2026-03-23 22:15:08 +0300

    feat: debug and secret key setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u)=(wq*yhtsih(3q!p@xesg^3+gtc-@pa&)e9d0g%par43^+ze"
+SECRET_KEY = get_env_variable("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_bool_env("DEBUG")
 
 ALLOWED_HOSTS = []
 
