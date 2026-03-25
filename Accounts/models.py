f1b9cfc8446a98bff932bbb98f40a30b05008704
Getonto3 <getontomoses18@gmail.com>
2026-03-25 23:53:34 +0300

    feat: User Model attributes

--- Accounts/models.py
+++ Accounts/models.py
@@ -8,3 +8,9 @@ class User(AbstractUser):
         ("volunteer", "Volunteer"),
         ("registrar", "Registrar"),
     )
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=Role_Choices, default="volunteer")
+
+    def __str__(self):
+        return self.full_name or self.username
