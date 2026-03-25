0ff2536cc5b416a5d5d44696c7216b04094a1632
Getonto3 <getontomoses18@gmail.com>
2026-03-25 23:52:26 +0300

    feat: User model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class User(AbstractUser):
+    Role_Choices = (
+        ("admin", "Admin"),
+        ("coordinator", "Coordinator"),
+        ("teacher", "Teacher"),
+        ("volunteer", "Volunteer"),
+        ("registrar", "Registrar"),
+    )
