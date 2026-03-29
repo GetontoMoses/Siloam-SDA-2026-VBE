cd4e64518725f23cffd5e8814255de61d3f1d73a
Getonto3 <getontomoses18@gmail.com>
2026-03-29 23:04:49 +0300

    feat: add a timestamp to guardian creation

--- VBS/models.py
+++ VBS/models.py
@@ -10,6 +10,7 @@ class Guardian(models.Model):
     home_church = models.CharField(max_length=255, blank=True, null=True)
     emergency_contact_name = models.CharField(max_length=255, blank=True, null=True)
     emergency_contact_phone = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.full_name
