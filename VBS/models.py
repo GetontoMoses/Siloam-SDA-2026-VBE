ba8f6f64a8d509f48b2034b44373edfc332d88dd
Getonto3 <getontomoses18@gmail.com>
2026-03-30 22:00:29 +0300

    feat: added timestamp to program model

--- VBS/models.py
+++ VBS/models.py
@@ -60,7 +60,8 @@ class VBSProgram(models.Model):
     venue = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="draft")
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.title
 
