798bb284df7786ef3efe973cc74158c8c1ab1457
Getonto3 <getontomoses18@gmail.com>
2026-04-01 22:53:48 +0300

    feat: registration serializer

--- VBS/serializers.py
+++ VBS/serializers.py
@@ -35,3 +35,9 @@ class AgeGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = AgeGroup
         fields = "__all__"
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    child_name = serializers.CharField(source="child.full_name", read_only=True)
+    program_title = serializers.CharField(source="program.title", read_only=True)
+    group_name = serializers.CharField(source="group.name", read_only=True)
