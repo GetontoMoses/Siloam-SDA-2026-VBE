12bc0a2144342dbdbcfad52030d5fe11ae8b78cf
Getonto3 <getontomoses18@gmail.com>
2026-04-02 23:42:57 +0300

    feat: lesson serializer

--- VBS/serializers.py
+++ VBS/serializers.py
@@ -56,3 +56,9 @@ class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
         fields = "__all__"
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
