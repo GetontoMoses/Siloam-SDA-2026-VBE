c5035d3594e9144576433e8b2fed480c8fa3fbfb
Getonto3 <getontomoses18@gmail.com>
2026-04-08 22:22:11 +0300

    feat: url imports

---
+++ VBS/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    GuardianListCreateView,
+    GuardianDetailView,
+    ChildListCreateView,
+    ChildDetailView,
+    VBSProgramListCreateView,
+    VBSProgramDetailView,
+    AgeGroupListCreateView,
+    AgeGroupDetailView,
+    RegistrationListCreateView,
+    RegistrationDetailView,
+    AttendanceListCreateView,
+    AttendanceDetailView,
+    LessonListCreateView,
+    LessonDetailView,
+    ActivityListCreateView,
+    ActivityDetailView,
+)
