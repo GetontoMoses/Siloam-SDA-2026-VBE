b1a21bb1ff470255a2c0e275292cc63e660b5b67
Getonto3 <getontomoses18@gmail.com>
2026-04-08 22:14:17 +0300

    feat: user list

--- Accounts/views.py
+++ Accounts/views.py
@@ -3,3 +3,9 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import User
 from .serializers import UserSerializer
+
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
