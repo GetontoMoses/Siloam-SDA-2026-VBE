ce001e5a315c3e2fe63c213c00f6ded2fd1e1543
Getonto3 <getontomoses18@gmail.com>
2026-04-09 21:49:12 +0300

    chore: removed duplicate DB settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,18 +17,13 @@ from config.settings.utils import get_bool_env, get_env_variable
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_bool_env("DEBUG")
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -45,7 +40,7 @@ INSTALLED_APPS = [
     #local apps
     "VBS",
     "Accounts",
-    "Chilren_Ministry"
+    "Children_Ministry"
 ]
 
 MIDDLEWARE = [
@@ -77,17 +72,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
