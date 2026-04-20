f370f6bfa78ff86d5641eeeb158b16d02b2d8c16
Getonto3 <getontomoses18@gmail.com>
2026-04-20 22:37:13 +0300

    refactor: Update serializers and models for VBS program

--- VBS/serializers.py
+++ VBS/serializers.py
@@ -2,13 +2,10 @@ from rest_framework import serializers
 from .models import (
     Guardian,
     Child,
-    VBSProgram,
-    AgeGroup,
-    Registration,
     Attendance,
-    Lesson,
-    Activity,
-)
+    Teacher,
+    Station
+    )
 
 
 class GuardianSerializer(serializers.ModelSerializer):
@@ -25,28 +22,6 @@ class ChildSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class VBSProgramSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VBSProgram
-        fields = "__all__"
-
-
-class AgeGroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AgeGroup
-        fields = "__all__"
-
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    child_name = serializers.CharField(source="child.full_name", read_only=True)
-    program_title = serializers.CharField(source="program.title", read_only=True)
-    group_name = serializers.CharField(source="group.name", read_only=True)
-
-    class Meta:
-        model = Registration
-        fields = "__all__"
-
-
 class AttendanceSerializer(serializers.ModelSerializer):
     child_name = serializers.CharField(
         source="registration.child.full_name",
@@ -58,13 +33,13 @@ class AttendanceSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Lesson
+        model = Teacher
         fields = "__all__"
 
 
-class ActivitySerializer(serializers.ModelSerializer):
+class StationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Activity
+        model = Station
         fields = "__all__"
