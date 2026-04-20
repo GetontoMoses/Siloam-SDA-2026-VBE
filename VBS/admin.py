9d19a73a6648fb485903b3b43160ac762fff1e1e
Getonto3 <getontomoses18@gmail.com>
2026-04-20 23:11:18 +0300

    chore: Update admin.py to register new models

--- VBS/admin.py
+++ VBS/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from VBS.models import Attendance, Child, Guardian, Station, Teacher
+
 # Register your models here.
+admin.site.register(Guardian)
+admin.site.register(Child)
+admin.site.register(Attendance)
+admin.site.register(Teacher)
+admin.site.register(Station)
