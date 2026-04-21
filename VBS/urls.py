699d35fee8e5dee9f11f585c85ac0b3407c813b6
Getonto3 <getontomoses18@gmail.com>
2026-04-21 23:06:43 +0300

    chore: urls update

--- VBS/urls.py
+++ VBS/urls.py
@@ -10,26 +10,19 @@ from .views import (
     TeacherDetailView,
     StationListCreateView,
     StationDetailView,
-
 )
+
 urlpatterns = [
-    # Guardians
-    path("guardians/", GuardianListCreateView.as_view(), name="guardian-list-create"),
+    path("guardians/", GuardianListCreateView.as_view(), name="guardian-list"),
     path("guardians/<int:pk>/", GuardianDetailView.as_view(), name="guardian-detail"),
-    # Children
-    path("children/", ChildListCreateView.as_view(), name="child-list-create"),
+    path("children/", ChildListCreateView.as_view(), name="child-list"),
     path("children/<int:pk>/", ChildDetailView.as_view(), name="child-detail"),
-    # Attendance
-    path(
-        "attendance/", AttendanceListCreateView.as_view(), name="attendance-list-create"
-    ),
+    path("attendance/", AttendanceListCreateView.as_view(), name="attendance-list"),
     path(
         "attendance/<int:pk>/", AttendanceDetailView.as_view(), name="attendance-detail"
     ),
-    # Teachers
-    path("teachers/", TeacherListCreateView.as_view(), name="teacher-list-create"),
+    path("teachers/", TeacherListCreateView.as_view(), name="teacher-list"),
     path("teachers/<int:pk>/", TeacherDetailView.as_view(), name="teacher-detail"),
-    # Stations
-    path("stations/", StationListCreateView.as_view(), name="station-list-create"),
+    path("stations/", StationListCreateView.as_view(), name="station-list"),
     path("stations/<int:pk>/", StationDetailView.as_view(), name="station-detail"),
 ]
